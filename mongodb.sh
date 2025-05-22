2e37c2fe34a07230b4650577d1ac4a920ae3a30b
Chandu154 <devarachandra4@gmail.com>
2025-05-22 11:37:40 +0530

    Roboshop shell script

--- mongodb.sh
+++ mongodb.sh
@@ -50,7 +50,7 @@ sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>> $LOGFILE
 
 VALIDATE $? "Edited MongoDB conf"
 
-systemctl rstart  mongodb &>> $LOGFILE
+systemctl restart  mongodb &>> $LOGFILE
 
 VALIDATE $? "start"
 
