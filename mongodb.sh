07ee60b710d0844a1ae84e2c2bcd39655c45f3b5
Chandu154 <devarachandra4@gmail.com>
2025-05-22 10:25:27 +0530

    Roboshop shell script

---
+++ mongodb.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+DATE=$(date +%F)
+SCRIPT_NAME=$($0)
+LOGDIR=/home/ec2-user/Shell_Script/shell_logs 
+LOGFILE=$LOGDIR/$SCRIPT_NAME-$DATE.log
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"   
+
+
+USER=$(id -u)
+
+if  [ $USER -ne 0 ]
+    then
+        echo "Run with root acess"
+        exit 1    
+fi
+
+
+sudo dnf install -y mongodb-org
+
+VALIDATE $? Install
+
+sudo systemctl start mongod
+
+VALIDATE $? start
+
+systemctl status  mongodb
+
+VALIDATE $? status
+
+
+
+
+VALIDATE ()
+
+if  [ $1 -ne 0 ]
+    then
+        echo "$2 $R failed $N"
+        exit 1
+    else
+        echo "$2 $G successfull $N"
+fi        
