20b994b9fc506d295cc6481189429bf2fa999400
Chandu154 <devarachandra4@gmail.com>
2025-05-23 14:52:27 +0530

    Roboshop shell script

--- catalogueImprove.sh
+++ catalogueImprove.sh
@@ -33,12 +33,6 @@ VALIDATE (){
 
 
 
-
-
-
-
-
-
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>> $LOGFILE
 VALIDATE $? "Downloaded devolper content"
 
@@ -64,7 +58,7 @@ done <<EOF
 id -u roboshop|useradd roboshop|User roboshop
 ls -ld /app|mkdir /app|/app directory
 test -f /tmp/catalogue.zip|curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip|Catalogue zip file
-test -d /app/catalogue|unzip /tmp/catalogue.zip -d /app|Unzipping catalogue
+ttest -f /app/package.json|unzip -o /tmp/catalogue.zip -d /app|Unzipping catalogue
 EOF
 
 
