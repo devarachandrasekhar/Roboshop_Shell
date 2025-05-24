be378dfc34652efc606e1f753cc74e0a19da6602
Chandu154 <devarachandra4@gmail.com>
2025-05-24 12:34:45 +0530

    Roboshop shell script

--- create_EC2_Route53.sh
+++ create_EC2_Route53.sh
@@ -29,7 +29,14 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
     echo "creating $i"
-   IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE  --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
+
+   IP_ADDRESS=$(aws ec2 run-instances \
+  --image-id $IMAGE_ID \
+  --instance-type $INSTANCE_TYPE \
+  --security-group-ids $SECURITY_GROUP_ID \
+  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" \
+  | jq -r '.Instances[0].PrivateIpAddress')
+  
   echo "created $i instance: $IP_ADDRESS"
 
    aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE --change-batch '
