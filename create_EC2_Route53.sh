a1dd89af50875e08f5171833edef1f565ab9391c
Chandu154 <devarachandra4@gmail.com>
2025-05-24 12:48:31 +0530

    Roboshop shell script

--- create_EC2_Route53.sh
+++ create_EC2_Route53.sh
@@ -30,11 +30,7 @@ do
     fi
     echo "creating $i"
 
-   IP_ADDRESS=$(aws ec2 run-instances \
-  --image-id $IMAGE_ID \
-  --instance-type $INSTANCE_TYPE \
-  --security-group-ids $SECURITY_GROUP_ID \
-  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" )
+   IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE  --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
 
   echo "created $i instance: $IP_ADDRESS"
 
